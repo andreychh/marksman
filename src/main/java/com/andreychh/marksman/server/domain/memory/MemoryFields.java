c3724e741622d6616a66046aca0201c6d793edc1
Andrey Chernykh <andreychernykh04@gmail.com>
2025-04-10 18:55:18 +0300

    chore: format Javadoc comment

--- src/main/java/com/andreychh/marksman/server/domain/memory/MemoryFields.java
+++ src/main/java/com/andreychh/marksman/server/domain/memory/MemoryFields.java
@@ -3,7 +3,8 @@ package com.andreychh.marksman.server.domain.memory;
 import com.andreychh.marksman.common.geometry.Point;
 
 /**
- * This is redundant as a single game field is usually used. It is utilized for consistency when creating objects for Environment.
+ * This is redundant as a single game field is usually used. It is utilized for
+ * consistency when creating objects for Environment.
  */
 public final class MemoryFields {
     private final DataSource dataSource;
