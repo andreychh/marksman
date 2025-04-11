8a6eda2a983316145da9583ce1d388bd0017ca84
Andrey Chernykh <andreychernykh04@gmail.com>
2025-04-12 00:20:07 +0300

    feat(client): add FXApplication and components for login and lobby

---
+++ src/main/java/com/andreychh/marksman/client/MessageDispatcher.java
@@ -0,0 +1,38 @@
+package com.andreychh.marksman.client;
+
+import com.andreychh.marksman.common.network.Message;
+import com.andreychh.marksman.common.network.MessageHandler;
+
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// todo: MessageBroker has -er suffix
+public final class MessageDispatcher implements MessageHandler {
+    private final Map<String, List<MessageHandler>> handlers = new HashMap<>();
+
+    public void addHandler(final String action, final MessageHandler handler) {
+        if (!handlers.containsKey(action)) {
+            handlers.put(action, new ArrayList<>());
+        }
+        handlers.get(action).add(handler);
+    }
+
+    public void removeHandler(final String action, final MessageHandler handler) {
+        if (!handlers.containsKey(action)) {
+            return;
+        }
+        handlers.get(action).remove(handler);
+    }
+
+    @Override
+    public void handle(final Message message, final OutputStream outputStream) {
+        String action = message.value("action");
+        if (!handlers.containsKey(action)) {
+            return;
+        }
+        handlers.get(action).forEach(h -> h.handle(message, outputStream));
+    }
+}
---
+++ src/main/java/com/andreychh/marksman/client/components/LoginComponent.java
@@ -0,0 +1,38 @@
+package com.andreychh.marksman.client.components;
+
+import com.andreychh.marksman.client.MessageDispatcher;
+import com.andreychh.marksman.common.network.Message;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public final class LoginComponent {
+    private final OutputStream outputStream;
+    private final MessageDispatcher messageDispatcher;
+
+    @FXML
+    private TextField nameTextFiled;
+    @FXML
+    private Label errorLabel;
+
+    public LoginComponent(final OutputStream outputStream, final MessageDispatcher messageDispatcher) {
+        this.outputStream = outputStream;
+        this.messageDispatcher = messageDispatcher;
+    }
+
+    @FXML
+    private void onJoinLobbyButtonAction(final ActionEvent event) {
+        try {
+            new Message()
+                    .with("action", "lobby.join")
+                    .with("player.name", this.nameTextFiled.getText())
+                    .writeTo(this.outputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
