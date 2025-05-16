b336edfb1ea6003038e9d6a47f5fca6c78843901
Andrey Chernykh <andreychernykh04@gmail.com>
2025-05-16 20:59:44 +0300

    feat: add JTSPolygon class

---
+++ server/src/main/java/marksman/server/domain/game/JTSPolygon.java
@@ -0,0 +1,32 @@
+package marksman.server.domain.game;
+
+import marksman.shared.geometry.Point;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Polygon;
+
+import java.util.List;
+
+public final class JTSPolygon {
+    private final List<Point> points;
+
+    public JTSPolygon(final List<Point> points) {
+        this.points = points;
+    }
+
+    public boolean intersects(final JTSPolygon other) {
+        return this.polygon().intersects(other.polygon());
+    }
+
+    public boolean contains(final JTSPolygon other) {
+        return this.polygon().contains(other.polygon());
+    }
+
+    private Polygon polygon() {
+        return new GeometryFactory().createPolygon(
+                this.points.stream()
+                        .map(p -> new Coordinate(p.x(), p.y()))
+                        .toArray(Coordinate[]::new)
+        );
+    }
+}
